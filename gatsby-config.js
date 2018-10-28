f82ac1e12dc45692922e396d2c5bcf443bb11b16
Kristóf Poduszló <kripod@protonmail.com>
2018-10-28 23:45:18 +0100

    Remove TODO from `gatsby-config.js`

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,5 @@ module.exports = {
     title: 'Gatsby Strict Starter',
     language: 'en',
   },
-  plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-typescript',
-    /* TODO:
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-strict',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
-    */
-  ],
+  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-typescript'],
 };
