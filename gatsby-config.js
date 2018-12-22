54eb59324c0256e7407fcf07a63b954e08e46e92
Kristóf Poduszló <kripod@protonmail.com>
2018-12-22 12:10:42 +0100

    Start using Typography.js to improve basic styling of text nodes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,5 +9,11 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-typescript',
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography',
+      },
+    },
   ],
 };
--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,9 +1,5 @@
 const theme = {
   breakpoints: [600, 1024, 1440, 1920],
-  Text: {
-    fontFamily: 'system-ui, sans-serif',
-    lineHeight: 1.5,
-  },
 };
 
 export default theme;
