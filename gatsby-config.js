665b753253128dd65eec9131aef730504b8dabd2
Kristóf Poduszló <kripod@protonmail.com>
2020-01-20 23:43:27 +0100

    Greatly simplify ESLint configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,6 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        /* eslint-disable @typescript-eslint/camelcase */
         name: 'Gatsby Strict Starter',
         short_name: 'starter',
         start_url: '/',
@@ -18,10 +17,9 @@ module.exports = {
         theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/favicon.png',
-        /* eslint-enable */
       },
     },
-    '@rhysforyou/gatsby-plugin-react-helmet-async',
+    'gatsby-plugin-react-helmet-async',
     'gatsby-plugin-typescript',
     'gatsby-plugin-emotion',
   ],
